4f4dc76944c25e5101e5c78dee6ed2927305bdec
bajiroots <dickyarmansyah007@gmail.com>
2022-11-11 15:34:03 +0800

    tambah side dan desain input biaya

---
+++ database/seeders/JenisCiptaanSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use App\Models\JenisPermohonan;
+
+class JenisCiptaanSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::insert("insert into ");
+    }
+}
