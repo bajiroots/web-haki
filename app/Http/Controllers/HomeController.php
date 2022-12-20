6a6e67a537955e9643d48327bb63226daec97bd1
bajiroots <dickyarmansyah007@gmail.com>
2022-12-20 15:50:36 +0800

    Tambah edit & delete di modal permohonan dan tambah home controller

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,26 +3,27 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
+use App\Models\Permohonan;
 
 class HomeController extends Controller
 {
-    /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
-    public function __construct()
-    {
-        $this->middleware('auth');
-    }
-
-    /**
-     * Show the application dashboard.
-     *
-     * @return \Illuminate\Contracts\Support\Renderable
-     */
     public function index()
     {
-        return view('home');
+        $jmlhUser = User::all()->count();
+        $jmlhPermohonan = Permohonan::all()->count();
+        return view('welcome', compact('jmlhUser', 'jmlhPermohonan'));
+    }
+    public function about()
+    {
+        return view('about');
+    }
+    public function unduhan()
+    {
+        return view('unduhan');
+    }
+    public function search()
+    {
+        return view('search');
     }
 }
